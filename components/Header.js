76d6b3d5a8b202800a2e4f726e03f05f6f88ae82
Anas <amanabbasi3660@gmail.com>
2021-11-01 22:43:18 +0530

    change complete layout

--- components/Header.js
+++ components/Header.js
@@ -1,57 +1,37 @@
+import Image from "next/image";
+
 import {
   HomeIcon,
   SearchIcon,
   UserIcon,
   UserGroupIcon,
   BellIcon,
-  ChatIcon,
-  ChevronDownIcon,
-  MenuIcon,
+  ViewGridAddIcon,
 } from "@heroicons/react/solid";
-import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+
+import { FlagIcon, PlayIcon, ShoppingCartIcon } from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className="bg-black z-30 sticky top-0 h-16 flex items-center justify-between px-2 shadow-md w-full overflow-hidden border-b">
+    <header className="">
       {/* left */}
       <div className="flex items-center">
-        <img
-          className="h-12 w-12"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcOjVKk9jHPz7WfIRnSSPftbQ02XG1jJrvMw&usqp=CAU"
-          alt=""
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
         />
-        <div className="mx-3 flex items-center bg-[#25272c] p-2 rounded-3xl">
-          <SearchIcon className="h-6 w-6 text-gray-500" />
-          <input
-            className="bg-transparent outline-none border-0 px-2"
-            type="text"
-            placeholder="Search Facebook"
-          />
+        <div className="flex items-center">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
         </div>
       </div>
-      {/* center */}
-      <div className="hidden lg:flex lg:w-[570px] lg:items-center lg:justify-between  mx-12">
-        <HomeIcon className="headerIcons text-blue-600" />
-        <UserGroupIcon className="headerIcons" />
-        <SubscriptionsIcon className="headerIcons" />
-        <HomeIcon className="headerIcons" />
-        <HomeIcon className="headerIcons" />
-      </div>
-      {/* Right */}
-      <div className="flex items-center">
-        <img
-          className="hidden xl:inline-flex w-9 h-9 rounded-full cursor-pointer"
-          src="https://pbs.twimg.com/profile_images/1428603821408747524/HAGiZnNl_400x400.jpg"
-          alt=""
-        />
 
-        <div className="ml-6 flex items-center space-x-4">
-          <UserIcon className="headerNav" />
-          <UserIcon className="headerNav" />
-          <UserIcon className="headerNav" />
-          <UserIcon className="headerNav" />
-        </div>
-      </div>
+      {/* center */}
+      <div className=""></div>
+      {/* Right */}
+      <div className=""></div>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import Feed from "../components/Feed";
 import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
-import Widgets from "../components/Widgets";
 
 export default function Home() {
   return (
@@ -13,11 +10,6 @@ export default function Home() {
       </Head>
       <Header />
       {/* body */}
-      <div className="flex justify-between max-w-screen-2xl mx-auto p-3">
-        <Sidebar className="" />
-        <Feed className="" />
-        <Widgets className="" />
-      </div>
     </div>
   );
 }
